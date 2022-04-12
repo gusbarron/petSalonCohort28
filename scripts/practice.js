075b71380dff7e44ba7a94f5c5324cb37db25766
Gustavo Barron <gustavo.h.barron@gmail.com>
2022-04-11 19:55:07 -0700

    class edits

---
+++ scripts/practice.js
@@ -0,0 +1,22 @@
+let studentFirstName = "Gustavo";
+let studentLastName = "Barron";
+let studentAge = 26;
+let studentIsActive = true;
+
+//Object
+let student1 = {
+  fistName: "Gustavo",
+  lastName: "Barron",
+  age: 26,
+  isActive
+}
+
+let student=[];
+
+function displayStudent(){
+  for(let i=0; i<studentIsActive.length; i++){
+    console.log(students[0]);
+    console.log(students[2]);
+    console.log(students[2]);
+  }
+}
