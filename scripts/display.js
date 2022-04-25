6512502887edd3b0b8581b2152830c7b9b649c33
Gustavo Barron <gustavo.h.barron@gmail.com>
2022-04-24 18:38:02 -0700

    HW1

---
+++ scripts/display.js
@@ -0,0 +1,34 @@
+//display the pets in list items
+function displayPet(){
+  let tmp="";//clear the field
+  for(let i=0;i<petSalon.pets.length;i++){
+      let pet = petSalon.pets[i];// get each pet
+      tmp+=`<li>${pet.name} --- ${pet.age}</li> `;//concatenation
+      console.log(tmp);
+  }
+  document.getElementById("pets").innerHTML=tmp;
+}
+// dislay the pets in cards
+function displayCards(){
+  let tmp="";
+  for(let i=0;i<petSalon.pets.length;i++){
+      let pet = petSalon.pets[i];
+      tmp+=`
+      <div id="${pet.id}" class="pet">
+          <div class="pet-header"> 
+              <h4>${pet.name}</h4>
+              <button onclick="deletePet(${pet.id});"> 🗑️ </button>
+          </div>
+          <label>Age: ${pet.age}</label>
+          <label>Gender: ${pet.gender}</label>
+          <label>Breed: ${pet.breed}</label>
+          <label>Owner: ${pet.owner}</label>
+          <label>Phone:${pet.phone}</label>
+      </div>
+      `;
+  }
+  document.getElementById("pets").innerHTML=tmp;
+}
+
+
+//display the pets in table
--- scripts/practice.js
+++ scripts/practice.js
@@ -1,22 +1,46 @@
-let studentFirstName = "Gustavo";
-let studentLastName = "Barron";
-let studentAge = 26;
-let studentIsActive = true;
+//let studentFirstName = "Gustavo";
+////let studentLastName = "Barron";
+//let studentAge = 26;
+//let studentIsActive = true;
 
 //Object
-let student1 = {
-  fistName: "Gustavo",
-  lastName: "Barron",
-  age: 26,
-  isActive
+//let student1 = {
+ // fistName: "Gustavo",
+ // lastName: "Barron",
+//  age: 26,
+//  isActive
+//}
+
+//let student=[];
+
+//function displayStudent(){
+ // for(let i=0; i<studentIsActive.length; i++){
+ //   console.log(students[0]);
+ //   console.log(students[2]);
+ //   console.log(students[2]);
+ // }
+//}
+
+
+function Student(fname, lname, age, isActive){
+  this.firstName = fname;
+  this.lastName = lname;
+  this.age = age;
+  this.isActive = isActive;
 }
 
-let student=[];
+let student1 = new Student("Astrid", "Batres-Guerrero", 25, true);
+let student2 = new Student("Alexis", "Aldrete", 25, true);
 
-function displayStudent(){
-  for(let i=0; i<studentIsActive.length; i++){
-    console.log(students[0]);
-    console.log(students[2]);
-    console.log(students[2]);
-  }
+console.log(student1, student2);
+
+function Car(make, model, year){
+  this.carMake = make;
+  this.carModel = model;
+  this.carYear = year;
 }
+let car1 = new Car("Ford", "Mustang", 1968);
+ console.log = (car1);
+
+
+
